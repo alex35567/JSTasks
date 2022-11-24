abea1b686917f5cc1fdca0e4b95b5ac03b15c8eb
Alex35567 <Alex35567@rambler.ru>
2022-11-24 21:40:24 +0500

    исправил дерганья слайдера - добавил flex-shrink:0

--- scripts/task-dom_1.js
+++ scripts/task-dom_1.js
@@ -12,16 +12,17 @@
 
       document.querySelector('#prev').onclick = function(event) {        
         pos += width;
-        if (pos>=0)pos=0;        
+        if (pos>=0)pos=0;                
+        list.style.transition = '  0.5s';
+        // list.style.marginLeft = pos + 'px';        
         list.style.marginLeft = pos + 'px';        
-        list.style.transition = '0.5s';
         event.preventDefault();
       };
   
       document.querySelector('#next').onclick = function(event) {        
         pos -= width;
         if (pos<=-width*(all_elem_count-view_count)) pos=-width*(all_elem_count-view_count);        
-        list.style.transition = '0.5s';
+        list.style.transition = ' 0.5s';        
         list.style.marginLeft = pos + 'px';        
         //  for (let k=pos; k<=width * count; k++){             
         //      list.style.marginLeft = pos + 'px';        
